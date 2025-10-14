70b963b171cce3f90616a4e7e7b633d411bcb907
LWAhonen <lauri.ahonen@diktamen.com>
2025-10-14 15:02:31 +0300

    Use per-platform includes

--- win32/VS2015/build.bat
+++ win32/VS2015/build.bat
@@ -1,3 +1,8 @@
+if EXIST  "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
+call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+) else (
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
+)
 
 msbuild opus-tools.sln -t:Rebuild -p:Configuration=Debug  -p:Platform=Win32 -m
 msbuild opus-tools.sln -t:Rebuild -p:Configuration=Release -p:Platform=Win32 -m
